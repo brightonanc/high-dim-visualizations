d37979dcb72ca75a187a73eb9b7b5740e58522fe
brighton <>
2024-09-01 01:22:40 -0400

    Added grassmann13-visualizer, refactored code to have shared resources

---
+++ grassmann13-visualizer.py
@@ -0,0 +1,391 @@
+from util import *
+
+import textwrap
+from dataclasses import dataclass, field
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import widgets
+
+@dataclass
+class Grassmannian13:
+    point_R3: np.ndarray = field(default_factory=lambda: np.array([1., 0, 0]))
+    def verify(self):
+        if not 3 == self.point_R3.shape[-1]:
+            return False
+        if not np.isrealobj(self.point_R3):
+            return False
+        if 1e-12 < np.max(np.abs(1. - np.linalg.norm(self.point_R3))):
+            return False
+        return True
+    def get_point_R3(self):
+        return self.point_R3
+    def get_embedding(self):
+        P = self.point_R3[..., :, None] * self.point_R3[..., None, :]
+        K = 1
+        N = 3
+        G = ((N / (K * (N - K)))**0.5) * (P - ((K / N) * np.eye(N)))
+        return G
+
+def theta_phi_to_point_R3(theta, phi):
+    return np.stack((
+        np.cos(theta)*np.ones_like(phi),
+        np.sin(theta)*np.cos(phi),
+        np.sin(theta)*np.sin(phi),
+    ), axis=-1)
+
+
+class App:
+
+    def __init__(self):
+        self.translation_tables = {
+            'toggle-loop': (
+                True,
+                False,
+            ),
+            'toggle-full-mfold': (
+                False,
+                True,
+            ),
+        }
+        self._refs_keep_alive = []
+        self.registry = {
+            'embedding-matrix': None,
+            'theta-slider': None,
+            'phi-slider': None,
+            'toggle-loop': None,
+        }
+        margin_fig_x = 0.01
+        margin_fig_y = 0.02
+        fontsize = 8
+        fig = plt.figure()
+        self.fig = fig
+        subfigs_height_ratios = [1, 4]
+        subfigs = fig.subfigures(
+            2, 1,
+            wspace=0,
+            hspace=0,
+            height_ratios=subfigs_height_ratios,
+        )
+        self.subfigs = subfigs
+        axd = {}
+        subfigs0_width_ratios = [1, 2, 2, 1]
+        axd.update(subfigs[0].subplot_mosaic(
+            [
+                ['embedding-matrix', 'theta-slider', 'theta-slider', 'point_R3-scene'],
+                ['embedding-matrix', 'phi-slider', 'phi-slider', 'point_R3-scene'],
+                ['embedding-matrix', 'toggle-loop', 'toggle-full-mfold', 'point_R3-scene'],
+            ],
+            width_ratios=subfigs0_width_ratios,
+            per_subplot_kw={'point_R3-scene': dict(projection='3d')},
+        ))
+        subfigs[0].subplots_adjust(
+            left=margin_fig_x,
+            right=1.-margin_fig_x,
+            top=1.-((sum(subfigs_height_ratios)/subfigs_height_ratios[0])*margin_fig_y),
+        )
+        tmp = subfigs[1].subplots(1, 2, subplot_kw=dict(projection='3d'))
+        subfigs[1].subplots_adjust(
+            left=margin_fig_x,
+            right=1.-margin_fig_x,
+            bottom=(sum(subfigs_height_ratios)/subfigs_height_ratios[1])*margin_fig_y,
+        )
+        axd['embed0'] = tmp[0]
+        axd['embed1'] = tmp[1]
+        self.axd = axd
+
+        # General
+        tmp = self._keep_alive(widgets.TextBox(
+            axd['embedding-matrix'], '',
+            textalignment='center',
+        ))
+        self.registry['embedding-matrix'] = tmp
+        tmp.label.set_fontsize(fontsize)
+        tmp = self._keep_alive(widgets.Slider(
+            axd['theta-slider'], r'$\theta/\pi$',
+            0., 1., valinit=0., valstep=0.05, orientation='horizontal',
+        ))
+        self.registry['theta-slider'] = tmp
+        tmp.label.set_fontsize(fontsize)
+        tmp.valtext.set_fontsize(fontsize)
+        tmp.on_changed(self.on_sliders_change)
+        tmp = self._keep_alive(widgets.Slider(
+            axd['phi-slider'], r'$\phi/\pi$',
+            0., 2., valinit=0., valstep=0.05, orientation='horizontal',
+        ))
+        self.registry['phi-slider'] = tmp
+        tmp.label.set_fontsize(fontsize)
+        tmp.valtext.set_fontsize(fontsize)
+        tmp.on_changed(self.on_sliders_change)
+        tmp = self._keep_alive(StateButton(axd['toggle-loop'], (
+            'Hide Mobius Boundary Loop',
+            'Show Mobius Boundary Loop',
+        )))
+        self.registry['toggle-loop'] = tmp
+        tmp.label.set_fontsize(fontsize)
+        tmp.on_clicked(self.toggle_loop)
+        tmp = self._keep_alive(StateButton(axd['toggle-full-mfold'], (
+            'Show Entire Gr(1,3) Manifold',
+            'Show Partial Mobius Strip',
+        )))
+        self.registry['toggle-full-mfold'] = tmp
+        tmp.label.set_fontsize(fontsize)
+        tmp.on_clicked(self.toggle_full_mfold)
+
+        # Initial Cofiguration
+        for ax_name in ['embed0', 'embed1', 'point_R3-scene']:
+            axd[ax_name].set_xticks([])
+            axd[ax_name].set_yticks([])
+            axd[ax_name].set_zticks([])
+        for ax_name in ['embed0', 'embed1']:
+            axd[ax_name].view_init(
+                elev=45,
+                azim=45,
+                roll=0,
+            )
+        self.init_data()
+
+    def _keep_alive(self, x):
+        self._refs_keep_alive.append(x)
+        return x
+
+    def toggle_loop(self, event):
+        self.render()
+
+    def toggle_full_mfold(self, event):
+        do_show_full_mfold = self.translation_tables['toggle-full-mfold'][
+            self.registry['toggle-full-mfold'].get_state()
+        ]
+        if do_show_full_mfold:
+            self.render(render_all=True)
+        else:
+            self.render()
+
+    def update_embedding_matrix(self):
+        matrix = self.get_Grassmannian13().get_embedding()
+        prec = 3
+        matrix[np.abs(matrix) < 10**(-prec)] = 0.
+        with np.printoptions(precision=prec):
+            self.registry['embedding-matrix'].set_val(str(matrix))
+
+    def get_Grassmannian13(self):
+        theta = np.pi * self.registry['theta-slider'].val
+        phi = np.pi * self.registry['phi-slider'].val
+        gr13 = Grassmannian13(theta_phi_to_point_R3(theta, phi))
+        return gr13
+
+    def render(self, render_all=False):
+        #if True:
+        #    self.axd['embed0'].clear()
+        #    self.axd['embed1'].clear()
+        #    wid = int((1-2*np.abs(0.5-self.registry['theta-slider'].val))*N_tp)
+        #    theta = np.linspace(0, 0.5*np.pi, N_tp+1)[:, None][N_tp-wid:, :]
+        #    phi = np.linspace(0, 2*np.pi, N_tp+1)[None, :]
+        #    gr13 = Grassmannian13(theta_phi_to_point_R3(theta, phi))
+        #    G = gr13.get_embedding()
+        #    G_0 = G[:, :(N_tp//4)+1]
+        #    for i, G_data in enumerate((
+        #        (G_0[..., 0, 0], G_0[..., 1, 1], G_0[..., 2, 2]),
+        #        (G[..., 1, 2], G[..., 0, 2], G[..., 0, 1]))
+        #    ):
+        #        self.axd[f'embed{i}'].plot_surface(
+        #            *G_data,
+        #            color=(0., 1., 0., 0.8),
+        #        )
+        do_show_full_mfold = self.translation_tables['toggle-full-mfold'][
+            self.registry['toggle-full-mfold'].get_state()
+        ]
+        num_grid = self.render_data['G'].shape[0]-1
+        if (not do_show_full_mfold) or render_all:
+            for artist in self.render_data['artists']['mfold']:
+                artist.remove()
+            self.render_data['artists']['mfold'] = []
+            if not do_show_full_mfold:
+                t = self.registry['theta-slider'].val
+                ind = int((1 - (2 * abs(0.5 - t))) * num_grid)
+            else:
+                ind = 0
+            G = self.render_data['G'][ind:]
+            G_0 = self.render_data['G_partial_embed0'][ind:]
+            for i, G_data in enumerate((
+                (G_0[..., 0, 0], G_0[..., 1, 1], G_0[..., 2, 2]),
+                (G[..., 1, 2], G[..., 0, 2], G[..., 0, 1]))
+            ):
+                self.render_data['artists']['mfold'].append(
+                    self.axd[f'embed{i}'].plot_surface(
+                        *G_data,
+                        color=(0., 1., 0., 0.3),
+                    )
+                )
+            p = self.render_data['p'][ind:]
+            self.render_data['artists']['mfold'].append(
+                self.axd['point_R3-scene'].plot_surface(
+                    p[..., 0], p[..., 1], p[..., 2],
+                    color=(0., 0., 0., 0.4),
+                )
+            )
+            self.render_data['artists']['mfold'].append(
+                self.axd['point_R3-scene'].plot_surface(
+                    -p[..., 0], -p[..., 1], -p[..., 2],
+                    color=(0., 0., 0., 0.2),
+                )
+            )
+        for artist in self.render_data['artists']['misc']:
+            artist.remove()
+        self.render_data['artists']['misc'] = []
+        do_show_loop = self.translation_tables['toggle-loop'][
+            self.registry['toggle-loop'].get_state()
+        ]
+        if do_show_loop:
+            theta = np.pi * self.registry['theta-slider'].val
+            phi = np.linspace(0, 2*np.pi, num_grid+1)
+            gr13 = Grassmannian13(theta_phi_to_point_R3(theta, phi))
+            G = gr13.get_embedding()
+            for i, G_data in enumerate((
+                (G[..., 0, 0], G[..., 1, 1], G[..., 2, 2]),
+                (G[..., 1, 2], G[..., 0, 2], G[..., 0, 1]))
+            ):
+                self.render_data['artists']['misc'].append(
+                    self.axd[f'embed{i}'].plot3D(
+                        *G_data,
+                        color=(1., 0., 1., 1.),
+                    )[0]
+                )
+            p = gr13.get_point_R3()
+            self.render_data['artists']['misc'].append(
+                self.axd['point_R3-scene'].plot3D(
+                    p[..., 0], p[..., 1], p[..., 2],
+                    color=(1., 0., 1., 1.),
+                )[0]
+            )
+            self.render_data['artists']['misc'].append(
+                self.axd['point_R3-scene'].plot3D(
+                    -p[..., 0], -p[..., 1], -p[..., 2],
+                    color=(1., 0., 1., 1.),
+                )[0]
+            )
+        gr13 = self.get_Grassmannian13()
+        G = gr13.get_embedding()
+        for i, G_data in enumerate((
+            (G[0, 0], G[1, 1], G[2, 2]),
+            (G[1, 2], G[0, 2], G[0, 1]),
+        )):
+            self.render_data['artists']['misc'].append(
+                self.axd[f'embed{i}'].plot3D(
+                    *G_data,
+                    color=(0., 0., 0., 1.),
+                    marker='o',
+                    markersize=10,
+                )[0]
+            )
+        p = gr13.get_point_R3()
+        vec = np.array([-1., 1.])
+        self.render_data['artists']['misc'].append(
+            self.axd['point_R3-scene'].plot3D(
+                p[0]*vec, p[1]*vec, p[2]*vec,
+                '-o',
+                color=(0., 0., 0., 1.),
+            )[0]
+        )
+        for ax_name in ['embed0', 'embed1', 'point_R3-scene']:
+            set_aspect_equal_3d(self.axd[ax_name])
+
+    def on_sliders_change(self, val):
+        self.update_embedding_matrix()
+        self.render()
+
+    def init_data(self):
+        num_grid = 4 * 40 # multiple of slider resolution of 2/0.05 = 40
+        theta = np.linspace(0, 0.5*np.pi, num_grid+1)[:, None]
+        phi = np.linspace(0, 2*np.pi, num_grid+1)[None, :]
+        gr13 = Grassmannian13(theta_phi_to_point_R3(theta, phi))
+        G = gr13.get_embedding()
+        G_partial_embed0 = G[:, :(num_grid//4)+1]
+        p = gr13.get_point_R3()
+        self.render_data = {
+            'G': G,
+            'G_partial_embed0': G_partial_embed0,
+            'p': p,
+            'artists': {
+                'mfold': [],
+                'misc': [],
+            }
+        }
+        self.update_embedding_matrix()
+        self.render(render_all=True)
+
+    def get_info_message(self):
+        msg_arr = [
+            '''\
+                This interactive renders the Grassmann(1, 3) manifold
+                (equivalent to Real Projective 3-space) as a 2-dimensional
+                submanifold of the 5-sphere using the following general
+                Grassmann(K, N) embedding (where P is the associated projection
+                matrix):
+            ''', '''\
+                >   G = ((N / (K * (N - K)))**0.5) * (P - (K / N) I)
+            ''', '''
+            ''', '''\
+                This embedding is visualized via a pair of 3-dimensional
+                renderings: a left axes for the diagonal elements of G and a
+                right axes for the off-diagonal elements of G. The matrix G is
+                also displayed in the top left of the interactive. This
+                visualization aims to help the user's intuition towards the
+                following facts:
+            ''', '''\
+                * There is a 'natural' embedding of Gr(1, 3) as a submanifold
+                of the 5-sphere.
+            ''', '''\
+                * The topology of Gr(1, 3) is that of a mobius strip whose
+                boundary is contracted to a single point. (Note: a fun
+                corrolary is that such a topology is only embeddable (without
+                self-intersection) in 5-dimensional or higher Euclidean space.
+                Hopefully this interactive makes clear why it's not possible in
+                4D, and why 6D is redundant (note the left axes traces a
+                2-dimensional surface, thus reducing the required dimension by
+                1))
+            ''', '''
+            ''', '''\
+                This interactive always plots at least a single point on the
+                Gr(1, 3) manifold. This point is controlled via spherical
+                coordinates (under the projective equivalence) with the two
+                sliders. A visualization of the selected projective space in 3D
+                is shown in the top right corner as a line. Due to the
+                projective equivalence nature of Gr(1, 3), all points may be
+                represented using only theta <= 0.5 pi. However, the slider
+                allows for theta to range from 0 to pi to allow for more
+                diverse visualizations of trajectories. To highlight this
+                projective behavior, the scene in the top right corner shades
+                the positive hemisphere darker than the negative hemisphere.
+            ''', '''
+            ''', '''\
+                In addition, there are two buttons which toggle certain visuals from being displayed: 
+            ''', '''\
+                * The boundary of the mobius strip defined by (pi/2 - x) <=
+                theta <= (pi/2 + x)
+            ''', '''\
+                * The entire Gr(1, 3) manifold, or only the mobius strip
+                mentioned above
+            ''', '''
+            ''', '''\
+                Users are encouraged to play amply with the sliders to
+                visualize various trajectories, deform mobius
+                strips/boundaries, and consider how the product of the two 3D
+                plots evades self-intersection of the Gr(1, 3) topology. Users
+                are also encouraged to consider the charts/projections one
+                could make to 'naturally' map this geometry, given its
+                beautiful structure.
+            ''',
+        ]
+        msg_arr = [textwrap.dedent(x) for x in msg_arr]
+        msg_arr = [textwrap.fill(x, width=120) for x in msg_arr]
+        return '\n'.join(msg_arr)
+
+    def run(self):
+        print(self.get_info_message())
+        plt.show()
+
+
+if __name__ == '__main__':
+    App().run()
+
--- hermitian-op-visualizer.py
+++ hermitian-op-visualizer.py
@@ -1,16 +1,14 @@
+from util import *
 
 import textwrap
 import functools
 from dataclasses import dataclass, field
 
 import numpy as np
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib import widgets
 from matplotlib.axes import Axes
 from mpl_toolkits.mplot3d.axes3d import Axes3D
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
-from matplotlib.transforms import TransformedPatchPath
 
 @dataclass
 class RealSymmetricOperator3:
@@ -39,53 +37,6 @@ class RealSymmetricOperator3:
         assert 3 == x.shape[-1]
         return np.sum(self.eigenvalues * (x ** 2), axis=-1)
 
-def make_simplex_triangles(granularity):
-    """
-    Creates a mesh of the simplex in 3D
-
-    granularity is an int, the larger it is the finer the mesh
-
-    returns vectors, triangles
-    """
-    def make_triangulation(point_arr, triangle_arr=None, refine_level=1):
-        def refine(point_arr, ind_arr, offset):
-            new_point_arr = [
-                0.5 * (point_arr[ind_arr[0]] + point_arr[ind_arr[1]]),
-                0.5 * (point_arr[ind_arr[1]] + point_arr[ind_arr[2]]),
-                0.5 * (point_arr[ind_arr[2]] + point_arr[ind_arr[0]]),
-            ]
-            new_triangle_arr = [
-                (offset+2, ind_arr[0], offset+0),
-                (offset+0, ind_arr[1], offset+1),
-                (offset+1, ind_arr[2], offset+2),
-                (offset+0, offset+1, offset+2),
-            ]
-            return new_point_arr, new_triangle_arr
-        if triangle_arr is None:
-            triangle_arr = [(0, 1, 2)]
-        for _ in range(refine_level):
-            old_triangle_arr = triangle_arr
-            triangle_arr = []
-            for triangle in old_triangle_arr:
-                new_point_arr, new_triangle_arr = refine(
-                    point_arr,
-                    triangle,
-                    len(point_arr)
-                )
-                point_arr += new_point_arr
-                triangle_arr += new_triangle_arr
-        return point_arr, triangle_arr
-    point_arr = [
-        np.array([1., 0, 0]),
-        np.array([0., 1, 0]),
-        np.array([0., 0, 1]),
-    ]
-    point_arr, triangle_arr = make_triangulation(point_arr, refine_level=granularity)
-    vectors = np.stack(point_arr, axis=0)
-    vectors /= np.linalg.norm(vectors, axis=-1, keepdims=True)
-    triangles = np.array(triangle_arr, dtype=np.int32)
-    return vectors, triangles
-
 def axis_angle_to_matrix(theta, v0, v1, v2):
     assert isinstance(theta, float)
     assert isinstance(v0, float)
@@ -104,91 +55,6 @@ def axis_angle_to_matrix(theta, v0, v1, v2):
         (np.sin(theta) * np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]]))
     )
 
-def set_aspect_equal_3d(ax):
-    """
-    Sets Axes3D to use equal scale for all dimensions. Unfortunate that there's
-    not an easy function already present in mplot3d.
-
-    Parameters
-    ----------
-    ax : Axes3D
-        The Axes3D to equalize.
-
-    Returns: none
-    """
-    ax.set_box_aspect(aspect=(1,1,1))
-    extent = np.array([
-        ax.get_xlim(),
-        ax.get_ylim(),
-        ax.get_zlim(),
-    ])
-    wid = 0.5 * np.max(extent[:,1] - extent[:,0])
-    ctr = 0.5 * (extent[:,1] + extent[:,0])
-    ax.set_xlim(ctr[0]-wid, ctr[0]+wid)
-    ax.set_ylim(ctr[1]-wid, ctr[1]+wid)
-    ax.set_zlim(ctr[2]-wid, ctr[2]+wid)
-
-class StateButton(widgets.Button):
-    def __init__(self, ax, label_data_arr, *args, **kwargs):
-        tmp = label_data_arr[0]
-        super().__init__(ax, tmp if isinstance(tmp, str) else tmp['text'], *args, **kwargs)
-        assert not hasattr(self, '_state')
-        assert all(isinstance(_, str) for _ in label_data_arr) or all(isinstance(_, dict) for _ in label_data_arr)
-        self.label_data_arr = label_data_arr
-        self._state = 0
-        self.on_clicked(self.increment_state)
-    def get_state(self):
-        return self._state
-    def set_state(self, state):
-        self._state = state
-        label_data = self.label_data_arr[self._state]
-        if isinstance(label_data, str):
-            self.label.set_text(label_data)
-        else:
-            self.label.set_text(label_data['text'])
-            self.label.set_color(label_data['color'])
-        self.canvas.draw_idle()
-        return self
-    def increment_state(self, event=None):
-        self.set_state((self._state + 1) % len(self.label_data_arr))
-
-class SliderStyle0(widgets.Slider):
-    def __init__(self, fontsize, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        assert 'horizontal' == self.orientation
-        self.track.set(x=0., y=0., width=1., height=0.5)
-        kwargs_parent = {k: v for k, v in kwargs.items() if k not in {
-            'ax', 'label', 'valmin', 'valmax', 'valinit', 'valfmt',
-            'closedmin', 'closedmax', 'slidermin', 'slidermax', 'dragging',
-            'valstep', 'orientation', 'initcolor', 'track_color',
-            'handle_style',
-        }}
-        self.poly.remove()
-        self.poly = self.ax.axvspan(self.valmin, self.valinit, 0., 0.5, **kwargs_parent)
-        initcolor = 'r'
-        if 'initcolor' in kwargs:
-            initcolor = kwargs['initcolor']
-        self.vline.remove()
-        self.vline = self.ax.axvline(self.valinit, 0, 1, color=initcolor, lw=1,
-                                    clip_path=TransformedPatchPath(self.track))
-        # NOTE: Fixing the ball with self._handle is too finnicky, so I don't
-        # worry about it
-        self._handle.set_zorder(3)
-        self.label.set_fontsize(fontsize)
-        self.label.set_verticalalignment('top')
-        self.label.set_horizontalalignment('left')
-        self.label.set_position((0, 1))
-        self.valtext.set_fontsize(fontsize)
-        self.valtext.set_verticalalignment('top')
-        self.valtext.set_horizontalalignment('right')
-        self.valtext.set_position((1, 1))
-    def set_val(self, val):
-        super().set_val(val)
-        xy = self.poly.xy
-        xy[2] = val, 0.5
-        xy[3] = val, 0.
-        self._handle.set_xdata([val])
-        self.poly.xy = xy
 
 class App:
 
